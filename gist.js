d3b1b98117b507c9e1731d2f8b6df21ca37e57e0
Andy McKay <andymckay@github.com>
2019-12-20 15:19:56 -0800

    Update gist.js

--- gist.js
+++ gist.js
@@ -3,7 +3,7 @@ let token = process.env.TOKEN;
 let fs = require('fs');
 let https = require('https');
 
-console.log(process.env);
+console.log(process.env.INPUT_GISTURL.split('ded'));
 console.log('Gist URL:', gistURL);
 let gistID = gistURL.split('/')[4]
 console.log('Gist ID is:', gistID);
