039bc99a6333c50169bb2dd08c2987c9965dc8ba
Andy McKay <andymckay@github.com>
2019-12-20 14:59:44 -0800

    Update gist.js

--- gist.js
+++ gist.js
@@ -1,1 +1,47 @@
+let gistURL = process.env.INPUT_GISTURL;
+let token = process.env.TOKEN;
+let fs = require('fs');
+let https = require('https');
 
+console.log('Gist URL:', gistURL);
+let gistID = gistURL.split('/')[4]
+console.log('Gist ID is:', gistID);
+
+let options = {
+    'headers': {
+        'Accept': 'application/vnd.github.v3+json',
+        'Authorization': `token ${token}`,
+        'Content-Type': 'application/json',
+        'User-Agent': 'actions/get-gist-action'
+    }
+}
+
+https.get(`https://api.github.com/gists/${gistID}`, options, (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log('Gotten gist successfully from GitHub.')
+    let parsed = JSON.parse(data);
+    let files = Object.values(parsed.files);
+    if (files.length != 1) {
+        console.log("Error: looking for one and only one file.")
+        return;
+    }
+    let file = files[0];
+    fs.writeFile(`/tmp/${file['filename']}`, file['content'],
+    function(err) {
+        if (err) throw err;
+        console.log(`Gist is written to /tmp/${file['filename']} successfully.`);
+        return file['filename'];
+    });
+  });
+
+}).on("error", (err) => {
+  console.log(`Error getting gist: ${err.message}`);
+});
